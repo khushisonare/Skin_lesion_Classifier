937bdf747ac1ae1dd747720ba7590a592ae461fd
Khushi Sonare <khushisonare235@gmail.com>
2025-06-12 08:10:38 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropou
 
 def build_model(input_shape, num_classes):
     model = Sequential([
-        Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        Conv2D(32, (3, 3), activation='relu', input_shape=(64,64,3),
         MaxPooling2D(2, 2),
         Conv2D(64, (3, 3), activation='relu'),
         MaxPooling2D(2, 2),
